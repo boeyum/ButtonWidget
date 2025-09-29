b40cac99e220e1856ce583954996416371ef17f9
Jan-Ivar Bøyum <78592574+boeyum@users.noreply.github.com>
2025-09-29 11:32:58 +0200

    Add files via upload

---
+++ widgets/buttontype1/api.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    async apiWidgetPost({ homey, query }) {
+        const selectedDeviceId = query.deviceId;
+
+        const driver = homey.drivers.getDriver("buttondev");
+        for (const device of await driver.getDevices()) {
+            if (selectedDeviceId === device.getId()) {
+                const selectedDevice = device;
+                let state = selectedDevice.getCapabilityValue('onoff');
+                if (state) {
+                    selectedDevice.setCapabilityValue('onoff', false);
+                } else {
+                    selectedDevice.setCapabilityValue('onoff', true);
+                }
+            }
+        }
+    },
+};
